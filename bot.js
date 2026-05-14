851d3f37fcdd5d5c6bdf502ea17a6cc4d06f52b6
Berserk0v <berserk93@gmail.com>
2026-05-14 16:23:01 +0300

    Update bot.js
    xxx

--- bot.js
+++ bot.js
@@ -1,1 +1,25 @@
+const TelegramBot = require("node-telegram-bot-api");
 
+const TOKEN = process.env.TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+const URL = "https://berserk0v.github.io/FMC/";
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+
+  const sent = await bot.sendMessage(chatId,
+`📊 КАЛЬКУЛЯТОР ТОПЛИВА`,
+  {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "📊 Открыть приложение",
+            web_app: { url: URL }
+          }
+        ]
+      ]
+    }
+  });
+});
