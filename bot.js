48dd5e7b9b111cf9f52f91183bd9013ea325743e
Berserk0v <berserk93@gmail.com>
2026-05-14 17:15:07 +0300

    Update bot.js
    xxx

--- bot.js
+++ bot.js
@@ -1,18 +1,21 @@
 const CHANNEL = "@your_channel_username";
+const URL = "https://berserk0v.github.io/FMC/";
 
-bot.onText(/\/post/, (msg) => {
-  bot.sendMessage(CHANNEL, "🚀 Калькулятор топливной смеси:", {
+bot.onText(/\/publish/, async (msg) => {
+
+  const sent = await bot.sendMessage(CHANNEL, "🚀 Калькулятор топливной смеси", {
     reply_markup: {
       inline_keyboard: [
         [
           {
             text: "📊 Открыть приложение",
-            web_app: {
-              url: "https://berserk0v.github.io/FMC/"
-            }
+            web_app: { url: URL }
           }
         ]
       ]
     }
   });
+
+  // закрепляем сообщение
+  await bot.pinChatMessage(CHANNEL, sent.message_id);
 });
