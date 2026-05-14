35477420bafd18ec6777466e21dc10755726ca39
Berserk0v <berserk93@gmail.com>
2026-05-14 16:44:08 +0300

    Update bot.js
    xxx

--- bot.js
+++ bot.js
@@ -1,26 +1,29 @@
-const TOKEN = process.env.TOKEN;
-const bot = new TelegramBot(TOKEN, { polling: true });
-import TelegramBot from "node-telegram-bot-api";
+const TelegramBot = require("node-telegram-bot-api");
 
 const TOKEN = process.env.TOKEN;
 
 if (!TOKEN) {
-  throw new Error("TOKEN not found in env");
+  console.error("TOKEN not found in env");
+  process.exit(1);
 }
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 const URL = "https://berserk0v.github.io/FMC/";
 
+console.log("Bot started");
+
 bot.on("message", (msg) => {
   bot.sendMessage(msg.chat.id, "📊 Калькулятор:", {
     reply_markup: {
-      inline_keyboard: [[
-        {
-          text: "Открыть",
-          web_app: { url: URL }
-        }
-      ]]
+      inline_keyboard: [
+        [
+          {
+            text: "Открыть",
+            web_app: { url: URL }
+          }
+        ]
+      ]
     }
   });
 });
